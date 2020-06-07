9b6f339af4a6712d29c1d5a7edf9331aa3238351
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-06-06 22:40:29 -0300

    Hero and Section 1

--- src/components/section1/index.js
+++ src/components/section1/index.js
@@ -7,32 +7,33 @@ const easy = require('../../images/easy.png')
 export default function Section1(){
     return(
         <div className={styles.section1}>
-            <div className={styles.planning}>
+            <div className={styles.sectionItem}>
                 <img src={planning} alt="Engenheiro planejando o projeto"/>
-                <div className={styles.section1Text}>
+                <div className={styles.sectionItemText}>
                     <h3>Planejamos</h3>
-                    <p>Nós dedicamos o tempo inicial do projeto para planejamento. Aalisamos os 
+                    <p>Nós dedicamos o tempo inicial do projeto para planejamento. Analisamos os 
                         potenciais problemas financeiros e estruturais antes da construção começar.</p>
                 </div>
             </div>
 
-            <div className={styles.collab}>
-                <div className={styles.collabText}>
+            <div className={styles.sectionItem}>
+                <div className={styles.sectionItemTextLeft}>
                     <h3>Colaboramos</h3>
                     <p>Possuímos uma lista de parceiros que são referência no que fazem, 
                         com quem nossos engenheiros e arquitetos gostam de trabalhar.</p>
                 </div>
-                <img src={collab} alt="Pessoas colaborando"/>
+                <div className={styles.rightImage}>
+                    <img src={collab} alt="Pessoas colaborando"/>
+                </div>
             </div>
 
-            <div className={styles.planning}>
+            <div className={styles.sectionItem}>
                 <img src={easy} alt="Aperto de mãos"/>
-                <div className={styles.section1Text}>
+                <div className={styles.sectionItemText}>
                     <h3>Facilitamos</h3>
                     <p>Temos formas de te ajudar a financiar o valor da obra e também para adiquirir seus móveis planejados.</p>
                 </div>
             </div>
-
         </div>
     )
 }

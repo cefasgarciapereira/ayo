bfd96390d383d62ee774f79ab0fff12f541b3606
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-06-21 17:44:42 -0300

    merge

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -8,10 +8,6 @@ export default function Hero(){
         <div className={styles.hero}>
             <div className={styles.gridBackground}>
             </div>
-<<<<<<< HEAD
-
-=======
->>>>>>> 9b6f339af4a6712d29c1d5a7edf9331aa3238351
             <div className={styles.gridContainer}>
                 <div className={styles.text}>
                     <h1>Conhecimento.</h1>
@@ -19,23 +15,16 @@ export default function Hero(){
                     Envolve combinar imaginação, visão e o conhecimento técnico para transformar uma obra 
                     em lar. Este é o diferencial da AYO.</p>
                 </div>
-<<<<<<< HEAD
             
-=======
->>>>>>> 9b6f339af4a6712d29c1d5a7edf9331aa3238351
                 <div className={styles.imageContainer}>
                     <img 
                     src={buildingImage} 
                     className={styles.building}
                     alt="Blue window building"/>
                 </div>
-<<<<<<< HEAD
             
                 <p className={styles.scroll}>Scroll</p>
             
-=======
-                <p className={styles.scroll}>Scroll</p>
->>>>>>> 9b6f339af4a6712d29c1d5a7edf9331aa3238351
                 <div className={styles.whiteArea}>
                 </div>
             </div>
